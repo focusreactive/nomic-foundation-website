331453066a08542411089f924472b7ee1c65256c
RavenInd <sam@focusreactive.com>
2023-02-14 18:07:03 +0400

    feat: add landing layout and theming boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const withLinaria = require('next-linaria');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const linariaConfig = withLinaria({
+  reactStrictMode: true,
+  linaria: {
+    cacheDirectory:
+      process.env.NODE_ENV === 'production'
+        ? '.next/cache/.linaria-cache'
+        : '.linaria-cache',
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['img.shields.io', 'hardhat.org'],
+  },
+});
+
+module.exports = withPlugins([linariaConfig, withBundleAnalyzer]);
+module.exports.linariaConfig = linariaConfig;
---
+++ src/types/commonTypes.ts
@@ -0,0 +1,4 @@
+export type SEOType = {
+  title: string;
+  description: string;
+};
