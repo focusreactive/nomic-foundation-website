c3a2622a77560d16f12dfe4ab144ce62fab3518e
RavenInd <sam@focusreactive.com>
2023-04-28 15:47:06 +0400

    Optimize fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const MeasurementID = process.env.NEXT_PUBLIC_MEASUREMENT_ID as string;
+
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/EncodeSans.woff2'
+          as='font'
+          type='font/woff2'
+          // crossOrigin='true'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${MeasurementID}');
+
+            `,
+          }}
+        />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
